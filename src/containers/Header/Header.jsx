e1dcfafb6dd8499df10ed3e2d0fcd6dc561ebe20
aguiarHugo <aguiarhugofx@gmail.com>
2023-02-23 18:16:22 -0300

    Grammar corrections

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
   return (
     <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
-      <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
-      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      <h1 className="gradient__text">Let's Build Something Amazing With GPT-3 OpenAI</h1>
+      <p>Yet bed any for traveling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
 
       <div className="gpt3__header-content__input">
         <input type="email" placeholder="Your Email Address" />
