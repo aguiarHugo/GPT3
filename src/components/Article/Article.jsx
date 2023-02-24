083f61ca37f7876edf6d5d1f565293b9ce4ab6cb
aguiarHugo <aguiarhugofx@gmail.com>
2023-02-23 21:18:41 -0300

    Styled Article component

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './Article.css'
 
-const Article = () => {
+const Article = ( { imgUrl, date, title }) => {
   return (
-    <div>Article</div>
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-img">
+        <img src={imgUrl} alt="Blog" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+          <div>
+            <p>{date}</p>
+            <h3>{title}</h3>
+          </div>
+          <p>Reade Full Article</p>
+        </div>
+    </div>
   )
 }
 
